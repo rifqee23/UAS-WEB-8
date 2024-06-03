9bee8dfbf1e8d2ab5dd7160aa81eebca79cb84a2
haech06 <akaruniaputri@gmail.com>
2024-06-03 22:56:28 +0700

    Push

---
+++ public/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gym";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
