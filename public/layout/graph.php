458aea691fa636b39f50cbcf2eb3bb5b316915ec
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-06 11:13:59 +0700

    Revert "Update"
    This reverts commit a18265ec0e477fa22a66e62cf490103ce1b6824d.

--- public/layout/graph.php
+++ public/layout/graph.php
@@ -1,8 +1,6 @@
-<div
-      class="relative flex flex-col text-gray-700 bg-white shadow-md rounded-xl bg-clip-border"
-    >
-     
-      <div class="px-2 pt-6 pb-0">
-        <div id="line-chart"></div>
-      </div>
-    </div>
+<div class="relative flex flex-col text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+
+  <div class="px-2 pt-6 pb-0">
+    <div id="line-chart"></div>
+  </div>
+</div>
