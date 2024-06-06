382e43a3d0e5b036808bac5b48fe705700ad2c63
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-06 11:41:59 +0700

    read data

--- public/layout/tableMember.php
+++ public/layout/tableMember.php
@@ -5,16 +5,6 @@ include __DIR__ . '/../database.php';
 $query = "SELECT * FROM member";
 $result = mysqli_query($conn, $query);
 
-// Periksa apakah query berhasil dieksekusi
-if ($result) {
-  // Ambil hasil query dan simpan dalam variabel $members
-  $member = mysqli_fetch_all($result, MYSQLI_ASSOC);
-} else {
-  echo "Error retrieving members data: " . mysqli_error($conn);
-}
-
-// Tutup koneksi ke database
-mysqli_close($conn);
 ?>
 
 
@@ -81,59 +71,76 @@ mysqli_close($conn);
               Gender
             </p>
           </th>
+
           <th class="p-4 border-y border-blue-gray-100 bg-blue-gray-50/50">
             <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+              Status
+            </p>
+          </th>
+          <th class="p-4 border-y border-blue-gray-100 bg-blue-gray-50/50">
+            <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+              Action
             </p>
           </th>
         </tr>
       </thead>
+      <tbody>
+        <?php if (mysqli_num_rows($result) > 0) : ?>
+          <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+              <td class="p-4 border-b border-blue-gray-50">
+                <div class="flex items-center gap-3">
 
-      <tr>
-        <td class="p-4 border-b border-blue-gray-50">
-          <div class="flex items-center gap-3">
+                  <div class="flex flex-col">
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                      <?= $row["name"] ?>
+                    </p>
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900 opacity-70">
+                      <?= $row["email"] ?>
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td class="p-4 border-b border-blue-gray-50">
+                <div class="flex flex-col">
+                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <?= $row["phone"] ?>
+                  </p>
 
-            <div class="flex flex-col">
-              <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                John Michael
-              </p>
-              <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900 opacity-70">
-                john@creative-tim.com
-              </p>
-            </div>
-          </div>
-        </td>
-        <td class="p-4 border-b border-blue-gray-50">
-          <div class="flex flex-col">
-            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-              082179315713
-            </p>
+                </div>
+              </td>
+              <td class="p-4 border-b border-blue-gray-50">
+                <div class="w-max">
+                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    <?= $row["tanggal"] ?>
+                  </p>
+                </div>
+              </td>
+              <td class="p-4 border-b border-blue-gray-50">
+                <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                  <?= $row["gender"] ?>
+                </p>
+              </td>
 
-          </div>
-        </td>
-        <td class="p-4 border-b border-blue-gray-50">
-          <div class="w-max">
-            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-              23/02/2003
-            </p>
-          </div>
-        </td>
-        <td class="p-4 border-b border-blue-gray-50">
-          <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-            Male
-          </p>
-        </td>
-        <td class="p-4 border-b border-blue-gray-50">
-          <button class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-4 h-4">
-                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z">
-                </path>
-              </svg>
-            </span>
-          </button>
-        </td>
-      </tr>
+              <td class="p-4 border-b border-blue-gray-50">
+                <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                  <?= $row["status"] ?>
+                </p>
+              </td>
 
+              <td class="p-4 border-b border-blue-gray-50">
+                <button class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+                  <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-4 h-4">
+                      <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z">
+                      </path>
+                    </svg>
+                  </span>
+                </button>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        <?php endif; ?>
       </tbody>
     </table>
   </div>
