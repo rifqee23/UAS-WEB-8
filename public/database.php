dbf1280af83e1f39b2b1b343f11188881acb0450
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-06 16:59:41 +0700

    fix edit

--- public/database.php
+++ public/database.php
@@ -11,5 +11,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+<<<<<<< Updated upstream
 
 ?>
+=======
+// echo "Connected successfully";
+>>>>>>> Stashed changes
--- public/layout/tableMember.php
+++ public/layout/tableMember.php
@@ -21,12 +21,17 @@ $result = mysqli_query($conn, $query);
       </div>
       <div class="flex flex-col gap-2 shrink-0 sm:flex-row">
 
+<<<<<<< Updated upstream
         <button id = "addMemberButton" class="flex select-none items-center gap-3 rounded-lg bg-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+=======
+        <a class="flex select-none items-center gap-3 rounded-lg bg-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" href="./memberAdd.php" type="button">
+>>>>>>> Stashed changes
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" stroke-width="2" class="w-4 h-4">
             <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z">
             </path>
           </svg>
           Add member
+<<<<<<< Updated upstream
         </button>
 
         <script>
@@ -34,6 +39,9 @@ $result = mysqli_query($conn, $query);
             window.location.href = "memberAdd.php"
           };
         </script>
+=======
+        </a>
+>>>>>>> Stashed changes
       </div>
     </div>
     <div class="flex flex-col items-center justify-between gap-4 md:flex-row">
