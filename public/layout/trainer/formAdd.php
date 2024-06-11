17ced956a75880113ed550a09ae345f1ffd61881
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-06-11 17:15:52 +0700

    rubah heading (#1)

--- public/layout/trainer/formAdd.php
+++ public/layout/trainer/formAdd.php
@@ -59,6 +59,7 @@
         </div>
         <div class="flex justify-center w-full mt-8">
           <div class="flex justify-end w-full max-w-lg ">
+
             <button type="submit" class="w-32 py-2 text-2xl font-semibold text-white bg-green-400 rounded-xl">Add</button>
           </div>
         </div>
