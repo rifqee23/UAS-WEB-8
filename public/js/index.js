f30e15717cf359f8dc9ab6e2765cf4121ddbe3d0
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-13 11:33:18 +0700

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,10 @@ const trainers = document.getElementById("trainers");
 const schedule = document.getElementById("schedule");
 const book = document.getElementById("book");
 
-
 const btnMembers = document.getElementById("btnMembers");
 const btnTrainers = document.getElementById("btnTrainers");
 const btnSchedule = document.getElementById("btnSchedule");
-
 const btnBook = document.getElementById("btnBook");
-
 
 const toggleIcons = document.getElementById("toggleIcon");
 const toggleIconTrainer = document.getElementById("toggleIconTrainer");
@@ -30,10 +27,8 @@ const scheduleList = document.getElementById("scheduleList");
 const scheduleAdd = document.getElementById("scheduleAdd");
 const scheduleEdit = document.getElementById("scheduleEdit");
 
-
 const bookList = document.getElementById("bookList");
 const bookAdd = document.getElementById("bookAdd");
-
 
 
 
@@ -57,12 +52,10 @@ btnMembers.addEventListener("click", () => {
 
     }
 
-
     if(!book.classList.contains("hidden")) {
         book.classList.add("hidden");
         toggleIconBook.src = "asset/icons/up.svg";
     }
-
 })
 
 btnTrainers.addEventListener("click", () => {
@@ -77,42 +70,6 @@ btnTrainers.addEventListener("click", () => {
     if(!members.classList.contains("hidden")) {
         members.classList.add("hidden");
         toggleIcon.src = "asset/icons/up.svg";
-
-
-
-    }
-
-    if(!schedule.classList.contains("hidden")) {
-        schedule.classList.add("hidden");
-        toggleIconSchedule.src = "asset/icons/up.svg";
-
-    }
-});
-
-btnSchedule.addEventListener("click", () => {
-    schedule.classList.toggle("hidden");
-
-    if (schedule.classList.contains("hidden")) {
-        toggleIconSchedule.src = "asset/icons/up.svg";
-    } else {
-        toggleIconSchedule.src = "asset/icons/down.svg";
-    }
-
-    if(!trainers.classList.contains("hidden")) {
-        trainers.classList.add("hidden");
-        toggleIconTrainer.src = "asset/icons/up.svg";
-
-    }
-
-    if(!members.classList.contains("hidden")) {
-        members.classList.add("hidden");
-        toggleIcon.src = "asset/icons/up.svg";
-
-    }
-    
-});
-
-
 
     }
 
@@ -163,16 +120,6 @@ btnBook.addEventListener("click", () => {
         toggleIconBook.src = "asset/icons/up.svg";
     } else {
         toggleIconBook.src = "asset/icons/down.svg";
-
-});
-
-btnSchedule.addEventListener("click", () => {
-    schedule.classList.toggle("hidden");
-
-    if (schedule.classList.contains("hidden")) {
-        toggleIconSchedule.src = "asset/icons/up.svg";
-    } else {
-        toggleIconSchedule.src = "asset/icons/down.svg";
     }
 
     if(!trainers.classList.contains("hidden")) {
@@ -187,15 +134,13 @@ btnSchedule.addEventListener("click", () => {
 
     }
 
-
     if(!schedule.classList.contains("hidden")) {
         schedule.classList.add("hidden");
         toggleIconSchedule.src = "asset/icons/up.svg";
 
     }
 })
-    
-});
+
 
 
 memberList.addEventListener("click", () => {
@@ -241,6 +186,7 @@ trainerAdd.addEventListener("click", () => {
 scheduleEdit.addEventListener("click", () => {
     window.location.href = "scheduleEdit.php?id=1";
 })
+
 
 
 
